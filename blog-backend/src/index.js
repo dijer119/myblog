cdfd79c394c96c181df17bea2b5a527bedd8b683
jn.park <jn.park@navercorp.com>
2018-03-02 05:12:17 +0900

    관리자 인증 추가.

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser');
+const session = require('koa-session')
 
 const api = require('./api')
 
@@ -12,7 +13,8 @@ const api = require('./api')
 
 const {
   PORT: port = 4000,
-  MONGO_URI: mongoURI
+  MONGO_URI: mongoURI,
+	COOKIE_SIGN_KEY: signKey,
 } = process.env
 
 mongoose.Promise = global.Promise// Node 의 Promise 를 사용 하도록 설정
@@ -29,6 +31,13 @@ router.use('/api', api.routes())
 
 // 라우터 적용전에, bodyParser 적용
 app.use(bodyParser());
+
+const sessionConfig = {
+  maxAge: 86500000, //하루
+}
+
+app.use(session(sessionConfig, app))
+app.keys = [signKey]
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes())
